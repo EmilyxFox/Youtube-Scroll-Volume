e8c86ccfde6f19ef650c26aed4c09560260a076b
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2024-12-27 05:35:53 +0100

    Made a v2. v2 includes hot-reloading settings in extension popup. Typescript development, and svelte components.

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+// import adapter from "@sveltejs/adapter-static";
+
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+//   kit: adapter({
+//     pages: 'dist',
+//     assets: 'dist',
+//     fallback: undefined,
+//     precompress: false,
+//     strict: true
+//   })
+}
